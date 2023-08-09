f880fe739cfca2e6087a1853584907b3906e4773
junggji <139104031+junggji@users.noreply.github.com>
2023-08-09 15:19:09 +0900

    Update 이미지 슬라이드.js

--- 이미지 슬라이드.js
+++ 이미지 슬라이드.js
@@ -1,6 +1,16 @@
-$(".disp").html(no + " / 4");
+let timer = setInterval("show()", 3000);
+let no = 1, position;
+function show() {
+    
+        if(no<4) {
+        no++;
+    }   else {
+        no=1;
+    }
+        $(".disp").html(no + " / 4");
         // position = no*1000;
         $(".img4").animate({"left": "-=1000px"}, 500, function(){
-            $(".img4 li:first").appendTo(".img4");
-            $(".img4").css("left", "+=1000px");
+            $(".img4 li:first").appendTo(".img4"); // 첫이미지를 맨 뒤로 옮기기
+            $(".img4").css("left", "+=1000px"); // 이미지가 앞으로 땡겨지니까 1000px 뒤로 미루기
         });
+    }
